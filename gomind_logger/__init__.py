d38948c5b4e8c35edbd95d83c1828dab53c83cdf
GDJeffersonCarvalho <jefferson.carvalho@grupodomini.com>
2024-03-05 10:58:53 -0300

    fix: problems with logger folder

--- gomind_logger/__init__.py
+++ gomind_logger/__init__.py
@@ -9,26 +9,25 @@ class Logger:
         import logging
         import os
 
-        if not filename:
-            current_time = datetime.datetime.now().strftime("%d-%m-%Y %Hh%Mm")
-            if folder and folder is str:
-                os.path.join(folder, f"process_logs_{current_time}.txt")
-            else:
-                self.filename = f"process_logs_{current_time}.txt"
-        else:
-            if folder and folder is str:
-                os.path.join(folder, filename)
+        if filename:
+            if folder:
+                self.filename = os.path.join(folder, filename)
+                if not os.path.exists(folder):
+                    os.makedirs(folder)
             else:
                 self.filename = filename
+        else:
+            current_time = datetime.datetime.now().strftime("%d-%m-%Y %Hh%Mm")
+            if folder:
+                self.filename = os.path.join(folder, f"process_logs_{current_time}.txt")
+                if not os.path.exists(folder):
+                    os.makedirs(folder)
+            else:
+                self.filename = f"process_logs_{current_time}.txt"
 
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
         formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-
-        if not os.path.isfile(self.filename):
-            os.makedirs(os.path.dirname(self.filename), exist_ok=True)
-            with open(self.filename, "w"):
-                pass
 
         file_handler = logging.FileHandler(self.filename, encoding="utf-8")
         file_handler.setFormatter(formatter)
